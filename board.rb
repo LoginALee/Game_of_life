a143ea93505f426c1c55a900813f3973ba955687
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2021-02-05 15:57:53 -0600

    Functional but ugly version

--- board.rb
+++ board.rb
@@ -1,25 +1,22 @@
 require_relative 'cell'
 
 class Board
-  attr_accessor :board, :grid
+  attr_accessor :board
 
   def initialize(columns, rows)
     @columns = columns
     @rows = rows
     @board =  self.makeBoard()
-    @grid = self.makeGrid()
   end
 
-  def makeBoard
-    array = Array.new(@columns)
-    for i in 0..array.length 
-      array[i] = Array.new(@rows)
-    end
-    return array
-  end
  
-  def makeGrid
-    grid = @board
+  def makeBoard
+    grid = Array.new(@columns) 
+
+    for i in 0..grid.length
+      grid[i] = Array.new(@rows)
+    end
+
     for i in 0..@columns
       for j in 0..@rows
         cell1 = Cell.new
@@ -36,11 +33,55 @@ class Board
     return grid 
   end
   
-  def drawBoard
-    for row in @board
+  def drawBoard(board)
+    for row in board
       p row 
     end
     puts "\n"
+  end
+
+  def nextBoard
+    nextBoard = self.makeBoard()
+    grid = nextBoard
+    prevBoard = @board 
+    #Live neighbors    
+    for i in 0..@columns
+      for j in 0..@rows
+        state = grid[i][j]
+         neighbors = self.countNeighbors(grid,i,j) 
+
+        if(state == 'X' && neighbors == 3)
+          nextBoard[i][j] = 'O'
+        elsif(state == 'O' && (neighbors < 2 || neighbors > 3))
+          nextBoard[i][j] = 'X'
+        else
+          nextBoard[i][j] = state
+        end 
+      end
+    end
+    return nextBoard
+  end
+
+  def countNeighbors(board, x, y)
+    sum = 0
+    for i in -1..2
+      for j in -1..2
+
+        col = (x + i + @columns) % @columns
+        row = (y + j + @rows) % @rows
+
+        if board[col][row] == 'O'
+          sum += 1
+        end
+
+      end
+    end
+    
+    if board[x][y] == 'O'
+      sum -= 1
+    end
+    
+    return sum
   end
 end
 
--- main.rb
+++ main.rb
@@ -2,4 +2,5 @@ require_relative 'cell'
 require_relative 'board'
 
 board1 = Board.new(4,4)
-board1.drawBoard()
+board1.drawBoard(board1.board)
+board1.drawBoard(board1.nextBoard())
